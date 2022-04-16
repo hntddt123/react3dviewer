2b17c682168b2559dac8294483f67b99591a3e29
Nientai Ho <hntddt123@gmail.com>
2022-04-16 10:38:28 -0500

    Refactored generateStars from useMemo

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -7,44 +7,46 @@ function getRandomIntFrom(min, max) {
   return Math.random() * (max - min) + min;
 }
 
+function generateStars(pointCount) {
+  const positions = [];
+  const colors = [];
+
+  for (let index = 0; index < pointCount; index++) {
+    const x0 = getRandomIntFrom(-1, 1);
+    const x1 = getRandomIntFrom(-1, 1);
+    const x2 = getRandomIntFrom(-1, 1);
+    const x3 = getRandomIntFrom(-1, 1);
+    const x0123Squared = (x0 ** 2)
+      + (x1 ** 2)
+      + (x2 ** 2)
+      + (x3 ** 2);
+
+    const x = (2 * ((x1 * x3) + (x0 * x2)))
+      / x0123Squared;
+    const y = (2 * ((x2 * x3) - (x0 * x1)))
+      / x0123Squared;
+    const z = (x0 ** 2 + x3 ** 2 - x1 ** 2 - x2 ** 2)
+      / x0123Squared;
+
+    if (x0123Squared >= 1
+    ) {
+      positions.push(x * 100);
+      positions.push(y * 100);
+      positions.push(z * 100);
+
+      colors.push(0.8);
+      colors.push(0.8);
+      colors.push(0.8);
+    }
+  }
+
+  return [new Float32Array(positions), new Float32Array(colors)];
+}
+
 export default function Stars({ pointCount }) {
   const starsRef = useRef();
 
-  const [positions, colors] = useMemo(() => {
-    const positions = [];
-    const colors = [];
-
-    for (let index = 0; index < pointCount; index++) {
-      const x0 = getRandomIntFrom(-1, 1);
-      const x1 = getRandomIntFrom(-1, 1);
-      const x2 = getRandomIntFrom(-1, 1);
-      const x3 = getRandomIntFrom(-1, 1);
-      const x0123Squared = (x0 ** 2)
-        + (x1 ** 2)
-        + (x2 ** 2)
-        + (x3 ** 2);
-
-      const x = (2 * ((x1 * x3) + (x0 * x2)))
-        / x0123Squared;
-      const y = (2 * ((x2 * x3) - (x0 * x1)))
-        / x0123Squared;
-      const z = (x0 ** 2 + x3 ** 2 - x1 ** 2 - x2 ** 2)
-        / x0123Squared;
-
-      if (x0123Squared >= 1
-      ) {
-        positions.push(x * 100);
-        positions.push(y * 100);
-        positions.push(z * 100);
-
-        colors.push(0.8);
-        colors.push(0.8);
-        colors.push(0.8);
-      }
-    }
-
-    return [new Float32Array(positions), new Float32Array(colors)];
-  }, [pointCount]);
+  const [positions, colors] = useMemo(() => generateStars(pointCount), [pointCount]);
 
   useFrame(() => {
     starsRef.current.rotation.y += 0.00005;
