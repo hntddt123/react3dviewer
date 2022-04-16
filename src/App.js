d425a8f64e9739eb4e05dae0891a4d87a1a04a05
Nientai Ho <hntddt123@gmail.com>
2022-04-15 22:49:15 -0500

    Move Plane and added Stars to Canvas

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Canvas } from 'react-three-fiber';
 import { OrbitControls } from '@react-three/drei';
 
 import './App.css';
-import Camera from './components/Camera';
 import Box from './components/Box';
 import Plane from './components/Plane';
 import Melon from './components/Melon';
+import Stars from './components/Stars';
 
+// TODO: 855988 6b4984 483475 2b2f77 141852 070b34 gradient
 function App() {
   return (
     <div className="App">
@@ -15,10 +16,11 @@ function App() {
         <h2>
           3D object viewer
         </h2>
-        <div style={{ width: '95%', height: 500, backgroundColor: 'black' }}>
+        <div style={{ width: '95%', height: 500, backgroundColor: '#000000' }}>
           <Canvas
+            shadows
             pixelRatio={window.devicePixelRatio}
-            camera={{ fov: 70, near: 0.2, far: 1000, position: [0, 1, 6] }}
+            camera={{ fov: 70, near: 0.2, far: 1000, position: [0, 1, 5] }}
           >
             <Suspense fallback={null}>
               <ambientLight intensity={0.1} />
@@ -26,7 +28,8 @@ function App() {
               <Melon />
               <Box position={[-4, 0, 0]} />
               <Box position={[4, 0, 0]} />
-              <Plane rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -2, -5]} />
+              <Stars pointCount={700} />
+              <Plane rotation={[-0.5 * Math.PI, 0, 0]} position={[0, -1.5, 0]} />
               <OrbitControls makeDefault target={[0, 0, 0]} />
             </Suspense>
           </Canvas>
